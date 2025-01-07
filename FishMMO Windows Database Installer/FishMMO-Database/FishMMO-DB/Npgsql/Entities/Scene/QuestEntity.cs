bacdc10cb47898e40b950e8cf0f4d61d4200cb0b
LPayne93 <leseanpayne01@gmail.com>
2025-01-07 02:58:09 -0500

    t5432qg564t2

--- FishMMO Windows Database Installer/FishMMO-Database/FishMMO-DB/Npgsql/Entities/Scene/QuestEntity.cs
+++
@@ -1,9 +0,0 @@
-﻿using System.ComponentModel.DataAnnotations.Schema;
-
-namespace FishMMO.Database.Npgsql.Entities
-{
-	[Table("quest", Schema = "fish_mmo_postgresql")]
-	public class QuestEntity
-	{
-	}
-}
